13e24ac3e04e75898219855ec128d7b28a252b00
Mate Papp <matepapp@icloud.com>
2019-08-02 20:45:54 +0200

    Implement button atom

--- src/atoms/button/button.tsx
+++ src/atoms/button/button.tsx
@@ -1,25 +1,108 @@
+import React, { HTMLProps } from 'react';
+import styled from 'styled-components';
 import {
   color,
   ColorProps,
+  compose,
   layout,
   LayoutProps,
   space,
   SpaceProps,
-  buttonStyle,
-  ButtonStyleProps,
-  compose,
+  typography,
+  TypographyProps,
 } from 'styled-system';
-import styled from 'styled-components';
 
-export type ButtonProps = LayoutProps & ButtonStyleProps & ColorProps & SpaceProps;
+import { colors } from '../../tokens';
+import { CSSStateSelector } from '../../utils/types';
+
+const backgroundColors: ButtonVariants = {
+  primary: colors.secondary,
+  secondary: colors.white,
+  tertiary: colors.white,
+};
+
+const textColors: ButtonVariants = {
+  primary: colors.white,
+  secondary: colors.secondary,
+  tertiary: colors.secondary,
+};
+
+const borders: Record<CSSStateSelector, ButtonVariants> = {
+  default: {
+    primary: 'none',
+    secondary: `2px solid ${colors.secondary}`,
+    tertiary: 'none',
+  },
+  hover: {
+    secondary: `2px solid ${colors.secondaryDark}`,
+    tertiary: `2px solid ${colors.secondaryLight}`,
+  },
+  active: {
+    primary: `2px solid ${colors.secondary}`,
+    secondary: `3px solid ${colors.secondaryDark}`,
+    tertiary: `2px solid ${colors.secondaryLight}`,
+  },
+  focus: {
+    primary: `2px solid ${colors.secondaryDark}`,
+    secondary: `3px solid ${colors.secondary}`,
+    tertiary: `2px solid ${colors.secondaryLight}`,
+  },
+};
+
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
+type ButtonVariants = Partial<Record<ButtonVariant, string>>;
+
+interface Props {
+  variant: ButtonVariant;
+  isEnabled: boolean;
+}
+
+export type ButtonProps = Props &
+  LayoutProps &
+  ColorProps &
+  SpaceProps &
+  TypographyProps &
+  HTMLProps<HTMLButtonElement>;
 
 const styledProps = compose(
   layout,
   color,
   space,
-  buttonStyle,
+  typography,
 );
 
-export const Button = styled.button<ButtonProps>`
+const StyledButton = styled.button<ButtonProps>`
+  border: ${({ variant }: ButtonProps) => borders['default'][variant]};
+  background-color: ${({ variant }: ButtonProps) => backgroundColors[variant]};
+  color: ${({ variant }: ButtonProps) => textColors[variant]};
+
+  &:hover {
+    border: ${({ variant }: ButtonProps) => borders['hover'][variant]};
+    background-color: ${({ variant }: ButtonProps) => variant === 'primary' && colors.secondaryDark};
+  }
+
+  &:active {
+    border: ${({ variant }: ButtonProps) => borders['active'][variant]};
+  }
+
+  &:focus {
+    border: ${({ variant }: ButtonProps) => borders['focus'][variant]};
+  }
+
+  transition-duration: 0.2s;
+  transition-timing-function: ease-in-out;
   ${styledProps}
 `;
+
+// ts-ignore to fix type inconsistency because of color
+// @ts-ignore
+export const Button = (props: ButtonProps) => <StyledButton {...props} />;
+
+Button.defaultProps = {
+  variant: 'primary',
+  isEnabled: true,
+  minWidth: '130px',
+  height: '40px',
+  px: 3,
+  py: 2,
+};
---
+++ src/tokens/color.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryLight: '#BECAEA',
+  primary: '#6A87D8',
+  primaryDark: '#303AB2',
+  secondaryLight: '#A4EDDD',
+  secondary: '#5ECCB9',
+  secondaryDark: '#1B8B80',
+  white: '#fff',
+};
