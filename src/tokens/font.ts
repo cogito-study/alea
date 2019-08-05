cb6c1ca411e5dc3dac5d14c8d10bd5bc45b8fc6c
Ádám Belákovics <beladi96@gmail.com>
2019-08-05 19:02:20 +0200

    finalized fonts and spaces

--- src/tokens/font.ts
+++ src/tokens/font.ts
@@ -1,8 +1,4 @@
 export const fonts = {
   fontSizes: [12, 14, 16, 20, 24, 32, 40, 48],
-  lineHeights: {
-    condensedUltra: 1,
-    condensed: 1.25,
-    default: 1.5,
-  },
+  lineHeights: [1.1, 1.2, 1.6],
 };
