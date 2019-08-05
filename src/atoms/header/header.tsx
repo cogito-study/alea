893612069b838acf38c0c80f41503aa8aa0952a2
Ádám Belákovics <beladi96@gmail.com>
2019-08-05 19:36:21 +0200

    Finalize header

--- src/atoms/header/header.tsx
+++ src/atoms/header/header.tsx
@@ -2,26 +2,7 @@ import styled from 'styled-components';
 import React, { HTMLProps } from 'react';
 import { color, ColorProps, compose, space, SpaceProps, typography, TypographyProps } from 'styled-system';
 
-const fontSizes = {
-  h1: '3em',
-  h2: '2.25em',
-  h3: '1.75em',
-  h4: '1.125em',
-};
-
-const lineHeights = {
-  h1: '1.05em',
-  h2: '1.25em',
-  h3: '1.25em',
-  h4: '1.22222222em',
-};
-
-interface HeaderType {
-  as: 'h1' | 'h2' | 'h3' | 'h4';
-}
-
 export type HeaderProps = SpaceProps & TypographyProps & ColorProps & HTMLProps<HTMLHeadingElement>;
-export type HeaderPropsWithHeaderType = HeaderProps & HeaderType;
 
 const styledProps = compose(
   color,
@@ -29,10 +10,7 @@ const styledProps = compose(
   typography,
 );
 
-const StyledHeader = styled.div<HeaderPropsWithHeaderType>`
-  font-size: ${({ as }: HeaderPropsWithHeaderType) => fontSizes[as]};
-  line-height: ${({ as }: HeaderPropsWithHeaderType) => lineHeights[as]};
-
+const StyledHeader = styled.div<HeaderProps>`
   ${styledProps}
 `;
 
@@ -45,8 +23,12 @@ export const H3 = (props: HeaderProps) => <StyledHeader as="h3" {...props} />;
 // @ts-ignore
 export const H4 = (props: HeaderProps) => <StyledHeader as="h4" {...props} />;
 
-const defaultProps = {};
-H1.defaultProps = defaultProps;
-H2.defaultProps = defaultProps;
-H3.defaultProps = defaultProps;
-H4.defaultProps = defaultProps;
+const defaultProps = {
+  fontFamily: 'Raleway, ',
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+};
+H1.defaultProps = { ...defaultProps, fontSize: [5, 7], lineHeight: 0 };
+H2.defaultProps = { ...defaultProps, fontSize: [4, 5], lineHeight: 1 };
+H3.defaultProps = { ...defaultProps, fontSize: [3, 4], lineHeight: 1 };
+H4.defaultProps = { ...defaultProps, fontSize: 3, lineHeight: 1 };
