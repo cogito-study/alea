0218694cc0e88d2f7a3b0080d170064a73f65143
Mate Papp <matepapp@icloud.com>
2019-08-06 16:58:46 +0200

    Import Raleway font into project

---
+++ src/theme/global.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Raleway:600,700&display=swap');
+    font-family: 'Raleway', sans-serif;
+  }
+`;
