da6c1c3ce3c3801876c0b01abec58074daa13491
Mate Papp <matepapp@icloud.com>
2019-08-06 18:13:12 +0200

    Add Nunito Sans to global style

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -3,6 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   body {
     @import url('https://fonts.googleapis.com/css?family=Raleway:600,700&display=swap');
-    font-family: 'Raleway', sans-serif;
+    @import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
   }
 `;
