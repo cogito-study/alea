781ddf3db25ff4c322cad8425dcca1ed3f5af676
Ádám Belákovics <beladi96@gmail.com>
2019-08-06 13:46:23 +0200

    Resolve requested changes

--- src/atoms/paragraph/paragraph.tsx
+++ src/atoms/paragraph/paragraph.tsx
@@ -1,28 +1,27 @@
 import styled from 'styled-components';
 import React, { HTMLProps } from 'react';
 import { color, ColorProps, compose, space, SpaceProps, typography, TypographyProps } from 'styled-system';
+import { fonts } from '../../tokens';
 
-const fontSizes = {
-  small: 0,
-  medium: 1,
-  large: 2,
+type ParagraphSize = 'large' | 'medium' | 'small';
+
+const fontSizes: Record<ParagraphSize, string> = {
+  small: `${fonts.fontSizes[0]}px`,
+  medium: `${fonts.fontSizes[1]}px`,
+  large: `${fonts.fontSizes[2]}px`,
 };
 
-const lineHeights = {
-  small: 1,
-  medium: 2,
-  large: 2,
+const lineHeights: Record<ParagraphSize, number> = {
+  small: fonts.lineHeights[3],
+  medium: fonts.lineHeights[2],
+  large: fonts.lineHeights[2],
 };
 
-interface ParagraphType {
-  paragraphSize: 'large' | 'medium' | 'small';
+interface Props {
+  paragraphSize: ParagraphSize;
 }
 
-export type ParagraphProps = ParagraphType &
-  SpaceProps &
-  TypographyProps &
-  ColorProps &
-  HTMLProps<HTMLParagraphElement>;
+export type ParagraphProps = Props & SpaceProps & TypographyProps & ColorProps & HTMLProps<HTMLParagraphElement>;
 
 const styledProps = compose(
   color,
