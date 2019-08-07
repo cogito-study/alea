2832f4c76820d1d67ccd5ea898444d4c3abb09c5
Mate Papp <matepapp@icloud.com>
2019-08-07 18:11:33 +0200

    Minor color improvements

--- src/tokens/color.ts
+++ src/tokens/color.ts
@@ -1,11 +1,32 @@
 export const colors = {
   colors: {
-    primaryLight: '#BECAEA',
-    primary: '#6A87D8',
-    primaryDark: '#303AB2',
-    accentLight: '#A4EDDD',
-    accent: '#5ECCB9',
-    accentDark: '#1B8B80',
+    primary: {
+      light: '#BECAEA',
+      normal: '#6A87D8',
+      dark: '#303AB2',
+    },
+    accent: {
+      light: '#A4EDDD',
+      normal: '#5ECCB9',
+      dark: '#1B8B80',
+    },
+    grey: {
+      light: {
+        1: '#9D9D9D',
+        2: '#C2C2C2',
+        3: '#E8E8E8',
+        4: '#FBFBFB',
+      },
+      normal: '#737373',
+      dark: {
+        1: '#5A5A5A',
+        2: '#505050',
+        3: '#2E2E2E',
+        4: '#1E1E1E',
+      },
+    },
+    // TODO: error, warning, positive, info
     white: '#fff',
+    transparent: '#ffffff00',
   },
 };
