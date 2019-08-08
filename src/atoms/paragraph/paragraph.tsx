5b32adbd08041d343ee56fd63d508ccee51f3953
Ádám Belákovics <beladi96@gmail.com>
2019-08-08 20:24:19 +0200

    Rework anchor and paragraph to work with dynamic themes

--- src/atoms/paragraph/paragraph.tsx
+++ src/atoms/paragraph/paragraph.tsx
@@ -1,27 +1,21 @@
-import styled from 'styled-components';
+import styled, { css, ThemeProps } from 'styled-components';
 import React, { HTMLProps } from 'react';
 import { color, ColorProps, compose, space, SpaceProps, typography, TypographyProps } from 'styled-system';
-import { fonts } from '../../tokens';
+
+import { Theme, theme } from '../../theme';
 
 type ParagraphSize = 'large' | 'medium' | 'small';
-
-const fontSizes: Record<ParagraphSize, string> = {
-  small: `${fonts.fontSizes[0]}px`,
-  medium: `${fonts.fontSizes[1]}px`,
-  large: `${fonts.fontSizes[2]}px`,
-};
-
-const lineHeights: Record<ParagraphSize, number> = {
-  small: fonts.lineHeights[3],
-  medium: fonts.lineHeights[2],
-  large: fonts.lineHeights[2],
-};
 
 interface Props {
   paragraphSize: ParagraphSize;
 }
 
-export type ParagraphProps = Props & SpaceProps & TypographyProps & ColorProps & HTMLProps<HTMLParagraphElement>;
+export type ParagraphProps = Props &
+  SpaceProps &
+  TypographyProps &
+  ColorProps &
+  ThemeProps<Theme> &
+  HTMLProps<HTMLParagraphElement>;
 
 const styledProps = compose(
   color,
@@ -29,9 +23,29 @@ const styledProps = compose(
   typography,
 );
 
+const smallStyle = ({ theme: { fontSizes, lineHeights } }: ParagraphProps) => css`
+  font-size: ${`${fontSizes[0]}px`};
+  line-height: ${lineHeights[3]};
+`;
+
+const mediumStyle = ({ theme: { fontSizes, lineHeights } }: ParagraphProps) => css`
+  font-size: ${`${fontSizes[1]}px`};
+  line-height: ${lineHeights[2]};
+`;
+
+const largeStyle = ({ theme: { fontSizes, lineHeights } }: ParagraphProps) => css`
+  font-size: ${`${fontSizes[2]}px`};
+  line-height: ${lineHeights[2]};
+`;
+
+const styles: Record<ParagraphSize, any> = {
+  small: smallStyle,
+  medium: mediumStyle,
+  large: largeStyle,
+};
+
 const StyledParagraph = styled.p<ParagraphProps>`
-  font-size: ${({ paragraphSize }: ParagraphProps) => fontSizes[paragraphSize]};
-  line-height: ${({ paragraphSize }: ParagraphProps) => lineHeights[paragraphSize]};
+  ${(props: ParagraphProps) => styles[props.paragraphSize](props)};
 
   ${styledProps}
 `;
@@ -40,6 +54,7 @@ const StyledParagraph = styled.p<ParagraphProps>`
 export const Paragraph = (props: ParagraphProps) => <StyledParagraph {...props} />;
 
 Paragraph.defaultProps = {
+  theme: theme,
   fontFamily: 'paragraph',
   paragraphSize: 'medium',
 };
