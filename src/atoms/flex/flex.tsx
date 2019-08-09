a7c08e07bb05684ddf1c070f365dbbad8a3cc55c
Mate Papp <matepapp@icloud.com>
2019-08-09 18:13:08 +0200

    Fix box and flex props type

--- src/atoms/flex/flex.tsx
+++ src/atoms/flex/flex.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import { compose, FlexProps, FlexboxProps, flexbox } from 'styled-system';
+import {
+  compose,
+  FlexBasisProps,
+  flexbox,
+  FlexboxProps,
+  FlexGrowProps,
+  FlexProps,
+  FlexShrinkProps,
+  OrderProps,
+  system,
+  AlignSelfProps,
+} from 'styled-system';
 import { Box, BoxProps } from '../box/box';
 
-export type FlexProps = FlexboxProps & BoxProps;
+export type FlexContainerProps = FlexboxProps & BoxProps;
+export type FlexItemProps = FlexProps & FlexGrowProps & FlexShrinkProps & FlexBasisProps & OrderProps & AlignSelfProps;
+
+export const flexItemProps = system({
+  flex: true,
+  flexGrow: true,
+  flexShrink: true,
+  flexBasis: true,
+  justifySelf: true,
+  alignSelf: true,
+  order: true,
+});
 
 const styledFlexProps = compose(flexbox);
 
-const StyledFlex = styled(Box)<FlexProps>`
+const StyledFlex = styled(Box)<FlexContainerProps>`
+  display: flex;
   ${styledFlexProps}
 `;
 
-export const Flex = (props: FlexProps) => <StyledFlex {...props} />;
+// @ts-ignore
+export const Flex = (props: FlexContainerProps) => <StyledFlex {...props} />;
