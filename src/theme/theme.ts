11095bd97a9bb1d33b14bb67c64045b8393a79be
Mate Papp <matepapp@icloud.com>
2019-08-15 19:49:45 +0200

    Create borders

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,7 @@
-import { colors, fonts, spaces } from '../tokens';
+import { colors, fonts, spaces, borders } from '../tokens';
 
 export const theme = {
+  ...borders,
   ...colors,
   ...spaces,
   ...fonts,
