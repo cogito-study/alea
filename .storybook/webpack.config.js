fa945d27e4e71d2a387821c06473345b7c322189
kiralybalint <kiraly.balint@simonyi.bme.hu>
2019-08-16 17:07:23 +0200

    Remove storysource addon

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,11 +7,6 @@ module.exports = ({ config, mode }) => {
     }
   })
   config.module.rules.push({
-    test: /\.stories\.jsx?$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
-  })
-  config.module.rules.push({
     test: /\.(ts|tsx)$/,
     exclude: /node_modules/,
     use: [
