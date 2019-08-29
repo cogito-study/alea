a0b53265205014067829de0baf2448fee8a0477c
Ádám Belákovics <beladi96@gmail.com>
2019-08-29 14:45:48 +0200

    Fix header and anchor colors

--- src/atoms/anchor/anchor.tsx
+++ src/atoms/anchor/anchor.tsx
@@ -1,10 +1,20 @@
+import { themeGet } from '@styled-system/theme-get';
 import styled, { ThemeProps, css } from 'styled-components';
 import React, { HTMLProps } from 'react';
 import { color, ColorProps, compose, space, SpaceProps, typography, TypographyProps } from 'styled-system';
 
 import { Theme, theme } from '../../theme';
 
-export type AnchorProps = SpaceProps & TypographyProps & ColorProps & ThemeProps<Theme> & HTMLProps<HTMLAnchorElement>;
+interface Props {
+  hoverColor?: string;
+}
+
+export type AnchorProps = Props &
+  SpaceProps &
+  TypographyProps &
+  ColorProps &
+  ThemeProps<Theme> &
+  HTMLProps<HTMLAnchorElement>;
 
 const styledProps = compose(
   color,
@@ -16,27 +26,33 @@ const defaultStyle = ({
   theme: {
     colors: { accent, grey },
   },
+  color,
+  hoverColor,
 }: AnchorProps) => css`
   color: ${grey.light['2']};
   text-decoration: none;
   cursor: not-allowed;
+
   :link,
   :visited {
-    color: ${accent.normal};
+    color: ${({ color }: AnchorProps) => themeGet(`colors.${color}`, accent.normal)};
     text-decoration: none;
     cursor: pointer;
   }
+
   :hover {
-    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : accent.dark)};
+    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : themeGet(`colors.${hoverColor}`, accent.dark))};
     text-decoration: none;
   }
+
   :focus {
-    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : accent.normal)};
+    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : themeGet(`colors.${color}`, accent.normal))};
     text-decoration: underline;
     outline: none;
   }
+
   :active {
-    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : accent.normal)};
+    color: ${({ href }: AnchorProps) => (!href ? grey.light['2'] : themeGet(`colors.${color}`, accent.normal))};
     text-decoration: none;
   }
 `;
--- src/atoms/heading/heading.stories.tsx
+++ src/atoms/heading/heading.stories.tsx
@@ -9,3 +9,11 @@ export const h1 = () => <H1>{text('Content', 'H1 Heading')}</H1>;
 export const h2 = () => <H2>{text('Content', 'H2 Heading')}</H2>;
 export const h3 = () => <H3>{text('Content', 'H3 Heading')}</H3>;
 export const h4 = () => <H4>{text('Content', 'H4 Heading')}</H4>;
+export const colorful = () => (
+  <>
+    <H1 color="primary.light">{text('Content1', 'H1 Heading')}</H1>
+    <H2 color="primary.dark">{text('Content2', 'H2 Heading')}</H2>
+    <H3 color="grey.dark.2">{text('Content3', 'H3 Heading')}</H3>
+    <H4 color="error.dark">{text('Content4', 'H4 Heading')}</H4>
+  </>
+);
