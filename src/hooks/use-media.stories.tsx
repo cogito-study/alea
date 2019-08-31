a975fbdb0bbdb5db72fb5e5fd8566dd76aa191f4
Mate Papp <matepapp@icloud.com>
2019-08-31 17:20:23 +0200

    Create useMedia hooks

---
+++ src/hooks/use-media.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box } from '../atoms';
+import { hook } from '../utils/structure';
+import { useSmallScreenSize, useWindowSizeType } from './use-media';
+/* eslint react-hooks/rules-of-hooks: 0 */
+
+export default { title: hook('useMedia') };
+
+export const smallScreen = () => {
+  const isSmallScreen = useSmallScreenSize();
+
+  return (
+    <Box bg="accent.normal" color="white">
+      {isSmallScreen ? 'small' : 'not small'}
+    </Box>
+  );
+};
+
+export const windowSizeType = () => {
+  const sizeType = useWindowSizeType();
+
+  return (
+    <Box bg="accent.normal" color="white">
+      {sizeType}
+    </Box>
+  );
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,4 @@ import { FlattenSimpleInterpolation } from 'styled-components';
 
 export type CSSStateSelector = 'default' | 'hover' | 'active' | 'focus';
 export type StyleFunction<T> = (props: T) => FlattenSimpleInterpolation;
+export type SizeType = 'xs' | 's' | 'm' | 'l' | 'xl';
