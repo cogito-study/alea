a25be5a46a86809d15c311d9c8e26086f12cf9ba
Ádám Belákovics <beladi96@gmail.com>
2019-09-12 14:31:34 +0200

    Fix tslint error

--- src/tokens/color.stories.tsx
+++ src/tokens/color.stories.tsx
@@ -9,26 +9,29 @@ const { colors } = theme;
 
 const allColors = () => (
   <Flex flexWrap="wrap" flexDirection="column">
-    {Object.keys(colors).map((name, index) => (
-      <Fragment key={index}>
-        <H2>{name}</H2>
-        <Flex flexWrap="wrap">
-          {typeof colors[name] !== 'string' ? (
-            Object.keys(colors[name]).map((colorIndex) => (
-              <Box key={colorIndex} mr={5} textAlign="center">
-                <Box backgroundColor={`${name}.${colorIndex}`} width="100px" height="100px" borderRadius="10px" />
-                <H4>{colorIndex}</H4>
+    {Object.entries(colors).map((color, index) => {
+      const [name, colorObject] = color;
+      return (
+        <Fragment key={index}>
+          <H2>{name}</H2>
+          <Flex flexWrap="wrap">
+            {typeof colorObject !== 'string' ? (
+              Object.keys(colorObject).map((colorIndex) => (
+                <Box key={colorIndex} mr={5} textAlign="center">
+                  <Box backgroundColor={`${name}.${colorIndex}`} width="100px" height="100px" borderRadius="10px" />
+                  <H4>{colorIndex}</H4>
+                </Box>
+              ))
+            ) : (
+              <Box textAlign="center">
+                <Box backgroundColor={`${name}`} width="100px" height="100px" borderRadius="10px" />
+                <H4>{name}</H4>
               </Box>
-            ))
-          ) : (
-            <Box textAlign="center">
-              <Box backgroundColor={`${name}`} width="100px" height="100px" borderRadius="10px" />
-              <H4>{name}</H4>
-            </Box>
-          )}
-        </Flex>
-      </Fragment>
-    ))}
+            )}
+          </Flex>
+        </Fragment>
+      );
+    })}
   </Flex>
 );
 
